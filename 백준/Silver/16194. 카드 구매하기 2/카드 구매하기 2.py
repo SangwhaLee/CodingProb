1433c4ba1f4de761a5e3d70b4d322594e32f2be9
SangwhaLee <69105050+SangwhaLee@users.noreply.github.com>
2023-07-18 07:43:42 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 124 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.py
@@ -0,0 +1,19 @@
+N = int(input())
+
+price = list(map(int, input().split()))
+
+dp = [0]*(N)
+
+dp[0] = price[0]
+
+for i in range(1,len(price)):
+    tmp = []
+    tmp.append(price[i])
+    for j in range((i+1)//2):
+        if (i+1) % (j+1) == 0:
+            tmp.append(dp[j]*((i+1)//(j+1)))
+        tmp.append(dp[j]+dp[(i+1)-(j+1)-1])
+    dp[i] = min(tmp)
+
+print(dp[N-1])
+
