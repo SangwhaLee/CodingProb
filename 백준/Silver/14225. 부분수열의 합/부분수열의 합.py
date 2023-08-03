17ed985d31e9b6d40243c0d2cbc441bf6d811882
SangwhaLee <69105050+SangwhaLee@users.noreply.github.com>
2023-08-03 23:47:42 +0900

    [Silver I] Title: 부분수열의 합, Time: 476 ms, Memory: 187508 KB -BaekjoonHub

---
+++ 백준/Silver/14225. 부분수열의 합/부분수열의 합.py
@@ -0,0 +1,14 @@
+n = int(input())
+num = list(map(int,input().split()))
+visited = [0]*10000000
+
+def dfs(idx,sum) :
+    if idx == n :
+       return
+    sum += num[idx]
+    visited[sum] = 1
+    dfs(idx+1, sum)
+    dfs(idx+1, sum-num[idx])
+
+dfs(0,0)
+print(visited[1:].index(0)+1)
